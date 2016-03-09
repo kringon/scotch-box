f46414765878e11a6231c0a84dc4e27d619dd9e5
unknown <T820082@TNA-07-7271.corp.telenor.no>
2016-03-09 23:04:49 +0100

    lagre endringer

--- public/View/minNavigasjon.php
+++ public/View/minNavigasjon.php
@@ -15,7 +15,7 @@
     <div class="navbar navbar-default navbar-fixed-top">
       <div class="container">
         <div class="navbar-header">
-          <a href="../Index.php" class="navbar-brand">TestingBank</a>
+          <a href="../index.php" class="navbar-brand">TestingBank</a>
           <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
